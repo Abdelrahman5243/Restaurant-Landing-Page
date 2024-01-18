7b4dd0d609702802d5ad008276b40afa9221eeba
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-01-18 13:40:28 +0200

    isElementInViewport function

--- src/Components/ScrollAnimation.js
+++ src/Components/ScrollAnimation.js
@@ -6,12 +6,10 @@ const ScrollAnimation = ({ children }) => {
   function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top < window.innerHeight && rect.top >= -rect.height
     );
   }
+  
 
   useEffect(() => {
     function handleScroll() {
