b299e8fc320c2158507f378492ec0e0dded29aa5
abdelrahmanrgab <127410127+abdelrahmanrgab@users.noreply.github.com>
2024-01-18 12:12:18 +0200

    .

--- src/Components/Work.js
+++ src/Components/Work.js
@@ -10,12 +10,10 @@ const Work = () => {
   function isElementInViewport(el) {
     const rect = el.getBoundingClientRect();
     return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+      rect.top < window.innerHeight && rect.top >= 0
     );
   }
+  
 
   useEffect(() => {
     function handleScroll() {
